87d8fbb2b68bb8709bf8868007a06cbcece738e5
Daffa Kurnia Fatah <daffa@itsavirus.com>
2024-01-29 11:22:27 +0800

    feat: :sparkles: Add hover interaction for radial tree chart

--- src/app/radialtree/RadialTreeChart.tsx
+++ src/app/radialtree/RadialTreeChart.tsx
@@ -44,7 +44,7 @@ const RadialTreeChart = () => {
         .attr("transform", `translate(${width / 2},${height / 2})`);
 
       // Create a root for the tree layout
-      const treeLayout = d3.tree<Node>().size([2 * Math.PI, radius - 180]);
+      const treeLayout = d3.tree<Node>().size([2 * Math.PI, radius - 200]);
 
       // Compute the new tree layout
       treeLayout(root);
@@ -90,26 +90,58 @@ const RadialTreeChart = () => {
         .attr(
           "transform",
           (d: any) => `rotate(${(d.x * 180) / Math.PI - 90})translate(${d.y},0)`
-        );
+        )
+        .style("opacity", 1)
+        .style("transition", "opacity 0.3s ease-in-out");
 
       nodes
+        .attr("id", (_, i: number) => `node-${i}`)
         .append("circle")
         .attr("r", 5)
         .style("fill", (d: any) =>
           d.data.color ? d.data.color : "transparent"
         );
 
+      // Draw node percentage bars
       nodes
         .append("rect")
         .attr("x", -5) // Position based on the text
         .attr("y", "-0.5em") // Align with the text vertically
-        .attr("width", (d: any) => (d.data.value / 100) * 75) // Set a fixed width or calculate based on text length
+        .attr("width", (d: any) => (d.data.value / 100) * (100 - 28) + 28) // Set a fixed width or calculate based on text length
         .attr("height", "1em") // Height based on text size
         .style("fill", (d: any) =>
           d.data.name !== "ROOT" && d.parent.data.name !== "ROOT"
             ? d.data.color
             : "transparent"
-        );
+        )
+        .style("transition", "opacity 0.3s ease-in-out");
+
+      // Draw node percentage labels
+      nodes
+        .append("text")
+        .attr("dy", ".31em")
+        .attr("x", (d: any) => {
+          let padding: number = 20;
+          return d.x < Math.PI === !d.children ? padding : -padding;
+        })
+        .attr("text-anchor", (d: any) =>
+          d.x < Math.PI === !d.children ? "end" : "start"
+        )
+        .attr("transform", (d: any) => (d.x >= Math.PI ? "rotate(180)" : null))
+        .text((d: any) =>
+          d.data.name !== "ROOT" && d.parent.data.name !== "ROOT"
+            ? d.data.value + "%"
+            : ""
+        )
+        .style("font-size", "8px")
+        .style("user-select", "none")
+        .style(
+          "text-shadow",
+          "2px 2px 3px white, 2px -2px 3px white, -2px 2px 3px white, -2px -2px 3px white"
+        )
+        .style("cursor", "default")
+        .style("opacity", 0)
+        .style("transition", "opacity 0.3s ease-in-out");
 
       // Draw node labels
       nodes
@@ -118,7 +150,7 @@ const RadialTreeChart = () => {
         .attr("x", (d: any) => {
           let padding: number;
           if (d.data.name !== "ROOT" && d.parent.data.name !== "ROOT") {
-            padding = (d.data.value / 100) * 75;
+            padding = (d.data.value / 100) * (100 - 28) + 28;
           } else {
             padding = 6;
           }
@@ -135,7 +167,30 @@ const RadialTreeChart = () => {
           "text-shadow",
           "2px 2px 3px white, 2px -2px 3px white, -2px 2px 3px white, -2px -2px 3px white"
         )
-        .style("cursor", "default");
+        .style("cursor", "default")
+        .style("transition", "opacity 0.3s ease-in-out");
+
+      // Add interaction of hovering over nodes
+      nodes
+        .on("mouseenter", (event: any, d: any) => {
+          const currentNode = d3.select(event.currentTarget);
+          d3.selectAll(".node").style("opacity", 0.3);
+          currentNode.style("opacity", 1);
+          currentNode.select("text").style("opacity", 1);
+          nodes.filter((node) => node === d.parent).style("opacity", 1);
+          if (d.children) {
+            d.children.forEach((child: any) => {
+              let childrenNode = nodes
+                .filter((node) => node === child)
+                .style("opacity", 1);
+              childrenNode.select("text").style("opacity", 1);
+            });
+          }
+        })
+        .on("mouseleave", () => {
+          d3.selectAll(".node").style("opacity", 1);
+          d3.selectAll(".node").select("text").style("opacity", 0);
+        });
     }
   }, []);
 
