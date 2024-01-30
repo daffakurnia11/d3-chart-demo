2bbd95a9b48252760270a80e9f494b9bef35ac60
Daffa Kurnia Fatah <daffa@itsavirus.com>
2024-01-30 13:46:08 +0800

    feat: :sparkles: Create a needle for gauge chart

--- src/app/gauge/GaugeChart.tsx
+++ src/app/gauge/GaugeChart.tsx
@@ -2,6 +2,30 @@ import React, { useRef, useEffect } from "react";
 import * as d3 from "d3";
 import chroma from "chroma-js";
 
+const needleImage = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="75"
+    height="454"
+    viewBox="0 0 75 454"
+    fill="none"
+  >
+    <path
+      d="M35.0434 1.32009C35.1462 0.103309 36.9244 0.0970937 37.0357 1.31313L74.4643 410.148C71.4469 392.463 56.0456 379 37.5 379C18.7721 379 3.25064 392.729 0.450353 410.671L35.0434 1.32009Z"
+      fill="#7B8794"
+    />
+    <path d="M74.9967 416L74.9962 415.958L75 416H74.9967Z" fill="#7B8794" />
+    <path
+      d="M0.00389531 415.954L0.00326517 416H0L0.00389531 415.954Z"
+      fill="#7B8794"
+    />
+    <path
+      d="M38 454C57.33 454 73 438.33 73 419C73 399.67 57.33 384 38 384C18.67 384 3 399.67 3 419C3 438.33 18.67 454 38 454Z"
+      fill="#7B8794"
+    />
+  </svg>
+);
+
 const GaugeChart = () => {
   const ref = useRef(null);
 
@@ -133,6 +157,51 @@ const GaugeChart = () => {
       .attr("stroke", "black")
       .attr("stroke-width", 2)
       .attr("transform", `rotate(${-90})`);
+
+    // Needle dimensions
+    const needleLength = outerRadius - 20; // Adjust as needed
+    const needleWidth = 5; // Adjust as needed
+
+    // Needle Value (set this to whatever value you need to point at)
+    const needleValue = 85; // Example value
+
+    // Create a group for the needle
+    const needleGroup = svg
+      .append("g")
+      .attr("class", "needle")
+      .attr("transform", `translate(0, 0)`);
+
+    // Draw the needle circle base
+    needleGroup
+      .append("circle")
+      .attr("cx", 0)
+      .attr("cy", 0)
+      .attr("r", needleWidth)
+      .attr("fill", "#464A4F");
+
+    // Draw the needle line
+    needleGroup
+      .append("line")
+      .attr("class", "needle")
+      .attr("x1", 0)
+      .attr("y1", 0)
+      .attr("x2", 0)
+      .attr("y2", -needleLength)
+      .attr("stroke", "#464A4F")
+      .attr("stroke-width", needleWidth);
+
+    // Rotate the needle
+    const rotationScale = d3
+      .scaleLinear()
+      .domain([0, 100])
+      .range([-Math.PI / 1.4, Math.PI / 1.4]);
+
+    const needleRotation = rotationScale(needleValue);
+
+    needleGroup
+      .transition()
+      .duration(1500) // Transition time in milliseconds
+      .attr("transform", `rotate(${(needleRotation * 180) / Math.PI})`);
   }, []);
 
   return <svg ref={ref} />;
