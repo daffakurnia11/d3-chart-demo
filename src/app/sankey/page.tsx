702e44e0b2d7524812b5a03cf87dffaec98f5561
Daffa Kurnia Fatah <daffa@itsavirus.com>
2024-06-14 13:42:58 +0700

    feat: :card_file_box: Implement new data from BE

--- src/app/sankey/page.tsx
+++ src/app/sankey/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import data from "./data.json";
+import data from "./sankey-test-data-1.json";
 import SankeyChart from "./SankeyChart";
 
 export default function SankeyPage() {
@@ -9,7 +9,7 @@ export default function SankeyPage() {
     <div className="w-screen min-h-screen flex items-center justify-center flex-col">
       <h1 className="text-3xl font-bold text-center">Sankey Chart Test</h1>
       <div className="flex justify-center items-center w-full h-full">
-        <SankeyChart data={data} />
+        <SankeyChart data={data as any} />
       </div>
     </div>
   );
